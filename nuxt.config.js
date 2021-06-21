2c3eb7d54cbe40b4614f9541756d786045d5cb66
nyker510 <takamail53@gmail.com>
2021-06-22 08:08:32 +0900

    [update] ogp tag

--- nuxt.config.js
+++ nuxt.config.js
@@ -19,7 +19,45 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' },
+      {
+        hid: 'description',
+        name: 'description',
+        content:
+          'nyk510 のポートフォリオ'
+      },
+      { hid: 'og:site_name', property: 'og:site_name', content: 'nyk510 portfolio' },
+      { hid: 'og:type', property: 'og:type', content: 'website' },
+      { hid: 'og:url', property: 'og:url', content: 'https://nyk510.github.io/portfolio/' },
+      { hid: 'og:title', property: 'og:title', content: 'nyk510 portfolio' },
+      {
+        hid: 'og:description',
+        property: 'og:description',
+        content:
+          'nyk510 のポートフォリオ'
+      },
+      {
+        hid: 'og:image',
+        property: 'og:image',
+        content: '/icon_400x400.png'
+      },
+      {
+        hid: 'twitter:card',
+        property: 'twitter:card',
+        content: 'summary_large_image'
+      },
+      {
+        hid: 'twitter:title',
+        property: 'twitter:title',
+        content: 'nyk510 portfolio'
+      },
+      {
+        hid: 'twitter:description',
+        property: 'twitter:description',
+        content:
+          'nyk510 のポートフォリオ'
+      }
+
     ],
     link: [
       { rel: 'icon', type: 'image/png', href: '/icon_400x400.png' },
